ddf8ba8e094855117283130fd4f04aeafb346a06
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-02 18:28:44 +0000

    fix: update author email

--- src/Diyan.php
+++ src/Diyan.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Nigatedev framework package.
  *
- * (c) Abass Ben Cheik <abass@todaysdev.com>
+ * (c) Abass Ben Cheik <abass@todaydevs.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -22,7 +22,7 @@ use Psr\Http\Message\ServerRequestInterface;
 /**
 * Diyan template render
 *
-* @author Abass Ben Cheik <abass@todaysdev.com>
+* @author Abass Ben Cheik <abass@todaydevs.com>
 */
 class Diyan extends DiyanNotFoundTemplate implements DiyanInterface
 {
--- src/DiyanInterface.php
+++ src/DiyanInterface.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Nigatedev framework package.
  *
- * (c) Abass Ben Cheik <abass@todaysdev.com>
+ * (c) Abass Ben Cheik <abass@todaydevs.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
